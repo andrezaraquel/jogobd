bd53d40c7158da1cee3de82033071ca6add9cc5a
Abner M. C. Araujo <abner.math.c@gmail.com>
2015-02-21 17:06:45 -0300

    ...

---
+++ modals/avancar.php
@@ -0,0 +1,16 @@
+<div class='modal fade' id='ModalAvancar' tabindex='1' role='dialog' aria-labelledby='mySmallModalLabel' aria-hidden='true' data-backdrop='static'>
+	<div class='modal-dialog modal-sm' style=' margin-top: 14%; background-color: rgb(48, 80, 128); border-radius: 4%;'>
+		<section class='modal-body' id='corpoModal'>
+		</section>
+		</br>
+		<div class="modal-footer" align ='center'>
+			<div class="col-sm-12">												
+				<div style="float:left; padding-right:2px;"><?php echo "<a href='https://twitter.com/share' class='twitter-share-button' data-url='http://mrdataanalyst.pe.hu' data-text='Minha pontuação foi de $salarioAtual' data-lang='en' data-count='none' data-hashtags='ResultadoMrDataAnalyst,MinhaPontuacao'>Tweet</a>";?>
+				</div>
+				<div style="float:left;padding-right:2px;"><div class="fb-share-button" data-href="http://mrdataanalyst.pe.hu" data-layout="button" data-lang="en"></div></div>
+				<div style="float:left; padding-right:2px;"><div class="g-plus" data-action="share" data-annotation="none" data-href="http://mrdataanalyst.pe.hu" data-lang="en"></div></div>
+			</div>
+			
+		</div>
+	</div>			
+</div>
